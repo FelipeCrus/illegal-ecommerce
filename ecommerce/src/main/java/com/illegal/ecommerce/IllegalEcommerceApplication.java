2410be588f71647738ada6efc95b7cbeb45eacde
Felipe <felipecrus2909@gmail.com>
2025-12-04 15:33:29 -0300

    commit with files I forgot to send

---
+++ ecommerce/src/main/java/com/illegal/ecommerce/IllegalEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.illegal.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IllegalEcommerceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IllegalEcommerceApplication.class, args);
+    }
+
+}
---
+++ ecommerce/src/main/java/com/illegal/ecommerce/repository/UserRepository.java
@@ -0,0 +1,10 @@
+package com.illegal.ecommerce.repository;
+
+import com.illegal.ecommerce.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByEmail(String email);
+}
